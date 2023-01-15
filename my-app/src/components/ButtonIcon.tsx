c730c8da26a7c326ea9d6c53f92afbe1ad28a96e
alcoforado <marcosalcoforado@gmail.com>
2023-01-15 18:36:15 -0500

    Delete my-app

--- my-app/src/components/ButtonIcon.tsx
+++
@@ -1,9 +0,0 @@
-interface IButtonIconProp {
-    icon?:string;
-}
-
-export default function ButtonIcon(props:IButtonIconProp) {
-    return (
-        <button  className="button-icon material-icons">{props.icon}</button>
-    )
-} 
